ca5252d36bf61eaead2ed303bbe8876e439af822
Doug Cole <doug@estately.com>
2018-04-16 16:18:27 -0700

    add question show page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'questions#index'
 
-  resources :questions, :only => [:index, :new, :create]
+  resources :questions, :only => [:index, :new, :create, :show]
   resources :candidates, :only => [:index, :show]
   resources :votes, :only => :create
   get 'search', to: 'search#index', as: :search
